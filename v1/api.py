7313c5e484c8aa43195eefb686f53c1e28726efa
ThisIsBBBGithubAc <imax7964@gmil.com>
2022-07-03 02:01:38 +0530

    URL-Shortener

--- v1/api.py
+++ v1/api.py
@@ -47,7 +47,8 @@ async def short_url(request: Request, original_url:str=Form(...), short_code:Uni
         if short_code is not None:
 
             short_code_existance = get_data_by_short_code(session=session, short_code = short_code)
-            print("###########", short_code_existance.short_code)
+            print("###########", short_code_existance)
+            print(type(short_code_existance))
 
             if short_code_existance is not None:
                 return {"status" : "Custom code already in use! Please try a different one."}
