857ab80e00c03df880b49d91b29478443e8d8e23
ThisIsBBBGithubAc <imax7964@gmil.com>
2022-07-03 03:30:54 +0530

    URL-Shortener

--- v1/api.py
+++ v1/api.py
@@ -52,7 +52,7 @@ async def short_url(request: Request, original_url:str=Form(...), short_code:Uni
             print(type("<class 'v1.model.UrlModel'>"))
 
             if str(type(short_code_existance)) == "<class 'v1.model.UrlModel'>":
-                print("Duplicate")
+                return "Custom Short Code already in use! Please try a different one."
                 # return {"status" : "Custom Short Code already in use! Please try a different one."}
 
         else:
